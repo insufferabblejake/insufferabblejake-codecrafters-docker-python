091ab002a973dd27885b09088554a4c739e6de7e
Karthik Jagadeesan <insufferablejake@gmail.com>
2023-07-10 11:54:34 -0700

    stage 2: add comments

--- app/main.py
+++ app/main.py
@@ -8,10 +8,20 @@ def main():
     args: list[str] = sys.argv[4:]
     # print(f"Command: {command} Args: {args}")
 
+    # This call does a lot of heavy lifting
+    # Specifically, it calls the Popen() lib call which will call a fork() and exec() thus
+    # executing the command (with it's args) in a new process space. Usual inheritance of
+    # stdout, stderr and stdin from the parent process apply. Including sigmask etc.
+    # the capture_output flag controls whether we pipe stdout and stderr back to parent
+    # so we can save it/process it later.
     completed_process = subprocess.run([command, *args], capture_output=True)
     # print(f"Child stdout: {stdout_output}, child stderr: {stderr_output}")
 
-    # Redirect the captured stdout and stderr to those of the parent
+    # Write the captured stdout and stderr to those of the parent
+    # Note that this is different from actually redirecting the output of the child, we save
+    # and explicitly write. If we had wanted to redirect, we'd get the stdout and stderr of
+    # the child, use os.dup() in the parent to 'realtime' redirect, and restored the parents
+    # original file handles for these streams after redirection is done.
     sys.stdout.write(completed_process.stdout.decode("utf-8"))
     sys.stderr.write(completed_process.stderr.decode("utf-8"))
 
