27473289d78b6125a211cb727d543fb4e4696968
Karthik Jagadeesan <insufferablejake@gmail.com>
2023-07-10 13:20:32 -0700

    stage 4: pass

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 import subprocess
 import sys
+import os
+import shutil
+import tempfile
 
 
 def main():
@@ -8,34 +11,55 @@ def main():
     args: list[str] = sys.argv[4:]
     # print(f"Command: {command} Args: {args}")
 
-    # This call does a lot of heavy lifting
-    # Specifically, it calls the Popen() lib call which will call a fork() and exec() thus
-    # executing the command (with it's args) in a new process space. Usual inheritance of
-    # stdout, stderr and stdin from the parent process apply. Including sigmask etc.
-    # the capture_output flag controls whether we pipe stdout and stderr back to parent
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    # print(f"Child stdout: {stdout_output}, child stderr: {stderr_output}")
+    tmp_dir = tempfile.mkdtemp(dir='/tmp')
+    # old_cwd = os.getcwd()
+    completed_process = None
+    try:
+        shutil.copy(command, tmp_dir)
+        # the copied command name without path in the new place
+        command = "./" + command.split('/')[-1]
 
-    # Write the captured stdout and stderr to those of the parent
-    # Note that this is different from actually redirecting the output of the child, we save
-    # and explicitly write. If we had wanted to redirect, we'd get the stdout and stderr of
-    # the child, use os.dup() in the parent to 'realtime' redirect, and restored the parents
-    # original file handles for these streams after redirection is done.
-    sys.stdout.write(completed_process.stdout.decode("utf-8"))
-    sys.stderr.write(completed_process.stderr.decode("utf-8"))
+        # Change root directory to the temporary directory
+        os.chroot(tmp_dir)
+        os.chdir('/')
 
-    # Interesting that we can also do the following, instead of what is being done
-    # later on in the code.
-    # try:
-    #     completed_process.check_returncode()
-    # except subprocess.CalledProcessError as e:
-    #     # print(f"Child error {e.returncode}")
-    #     sys.exit(e.returncode)
+        # This call does a lot of heavy lifting
+        # Specifically, it calls the Popen() lib call which will call a fork() and exec() thus
+        # executing the command (with it's args) in a new process space. Usual inheritance of
+        # stdout, stderr and stdin from the parent process apply. Including sigmask etc.
+        # the capture_output flag controls whether we pipe stdout and stderr back to parent
+        completed_process = subprocess.run([command, *args], capture_output=True)
+        # print(f"Child stdout: {stdout_output}, child stderr: {stderr_output}")
 
-    # Check if child was naughty. If it was, hang head in shame!
-    if completed_process.returncode > 0:
-        sys.exit(completed_process.returncode)
-    sys.exit(0)
+        # Write the captured stdout and stderr to those of the parent
+        # Note that this is different from actually redirecting the output of the child, we save
+        # and explicitly write. If we had wanted to redirect, we'd get the stdout and stderr of
+        # the child, use os.dup() in the parent to 'realtime' redirect, and restored the parents
+        # original file handles for these streams after redirection is done.
+        sys.stdout.write(completed_process.stdout.decode("utf-8"))
+        sys.stderr.write(completed_process.stderr.decode("utf-8"))
+
+    finally:
+        # Change root back and restore original working dir
+        os.chroot('/')
+        # os.chdir(old_cwd)
+
+        # Remove the temporary directory and all its contents
+        # This doesn't seem to work, as cc runs the tests in a docker, so not sure what's happening.
+        # shutil.rmtree(tmp_dir)
+
+        # Interesting that we can also do the following, instead of what is being done
+        # later on in the code.
+        # try:
+        #     completed_process.check_returncode()
+        # except subprocess.CalledProcessError as e:
+        #     # print(f"Child error {e.returncode}")
+        #     sys.exit(e.returncode)
+
+        # Check if child was naughty. If it was, hang head in shame!
+        if completed_process is not None and completed_process.returncode > 0:
+            sys.exit(completed_process.returncode)
+        sys.exit(0)
 
 
 if __name__ == "__main__":
