701e6bf94560f9311e3799dc1431eb8313bc52d2
Karthik Jagadeesan <insufferablejake@gmail.com>
2023-07-10 16:33:49 -0700

    stage 5: pass rewrite

--- app/main.py
+++ app/main.py
@@ -6,76 +6,90 @@ import shutil
 import tempfile
 
 
-CLONE_NEWPID = 0x20000000
+CLONE_NEWPID: int = 0x20000000
+ERROR_CODE: int = 1
+LAST_INDEX: int = -1
+TEMP_DIR: str = '/tmp'
+ROOT_DIR: str = '/'
+LIBC = ctypes.CDLL(None)
+
+
+def copy_to_tmp_get_name(command: str) -> (str, str):
+    tmp_dir = tempfile.mkdtemp(dir=TEMP_DIR)
+    shutil.copy(command, tmp_dir)
+    return "./" + command.split('/')[LAST_INDEX], tmp_dir
+
+
+def isolate_child_fs(tmp_dir: str) -> None:
+    os.chroot(tmp_dir)
+    os.chdir(ROOT_DIR)
+
+
+def create_pid_namespace() -> None:
+    # ref. https://elixir.bootlin.com/linux/latest/source/include/uapi/linux/sched.h#L32
+    # ref. https://man7.org/linux/man-pages/man7/pid_namespaces.7.html
+    # The first process created after a call to unshare(2) using CLONE_NEWPID has the PID 1
+    # See also ref. https://man7.org/linux/man-pages/man2/unshare.2.html and
+    LIBC.unshare(CLONE_NEWPID)
+
+
+def exec_command(command: str, args: list[str]):
+    # This call does a lot of heavy lifting
+    # Specifically, it calls the Popen() lib call which will call a fork() and exec() thus
+    # executing the command (with it's args) in a new process space. Usual inheritance of
+    # stdout, stderr and stdin from the parent process apply. Including sigmask etc.
+    # the capture_output flag controls whether we pipe stdout and stderr back to parent
+    return subprocess.run([command, *args], capture_output=True)
+
+
+def get_stdio(subprocess_result) -> None:
+    # Write the captured stdout and stderr to those of the parent
+    # Note that this is different from actually redirecting the output of the child, we save
+    # and explicitly write. If we had wanted to redirect, we'd get the stdout and stderr of
+    # the child, use os.dup() in the parent to 'realtime' redirect, and restored the parents
+    # original file handles for these streams after redirection is done.
+    if subprocess_result.stdout:
+        sys.stdout.write(subprocess_result.stdout.decode("utf-8"))
+    if subprocess_result.stderr:
+        sys.stderr.write(subprocess_result.stderr.decode("utf-8"))
 
 
 def main():
-    libc = ctypes.CDLL(None)
-    # print(f"Command line: {sys.argv}")
-    command: str = sys.argv[3]
     args: list[str] = sys.argv[4:]
-    # print(f"Command: {command} Args: {args}")
 
-    tmp_dir = tempfile.mkdtemp(dir='/tmp')
-    # old_cwd = os.getcwd()
-    completed_process = None
+    subprocess_result = None
     try:
-        shutil.copy(command, tmp_dir)
-        # the copied command name without path in the new place
-        command = "./" + command.split('/')[-1]
+        command, tmp_dir = copy_to_tmp_get_name(sys.argv[3])
+        isolate_child_fs(tmp_dir)
+        create_pid_namespace()
+        subprocess_result = exec_command(command, args)
+        get_stdio(subprocess_result)
 
-        # Change root directory to the temporary directory
-        # fs isolation
-        os.chroot(tmp_dir)
-        os.chdir('/')
+    # Handle case where file ops in shutil fail
+    except OSError as e:
+        print(f"OS Error: {e}")
+        sys.exit(ERROR_CODE)
 
-        # process isolation
-        # Creating a new PID namespace
-        # ref. https://elixir.bootlin.com/linux/latest/source/include/uapi/linux/sched.h#L32
-        # ref. https://man7.org/linux/man-pages/man7/pid_namespaces.7.html
-        # The first created after a call to unshare(2) using CLONE_NEWPID has the PID 1
-        # See also ref. https://man7.org/linux/man-pages/man2/unshare.2.html and
-        libc.unshare(CLONE_NEWPID)
+    # Handle case where running the subprocess fails
+    # Note that this exception is also available as
+    except subprocess.CalledProcessError as e:
+        print(f"Child process error: {e}")
+        sys.exit(e.returncode)
 
-        # This call does a lot of heavy lifting
-        # Specifically, it calls the Popen() lib call which will call a fork() and exec() thus
-        # executing the command (with it's args) in a new process space. Usual inheritance of
-        # stdout, stderr and stdin from the parent process apply. Including sigmask etc.
-        # the capture_output flag controls whether we pipe stdout and stderr back to parent
-        completed_process = subprocess.run([command, *args], capture_output=True)
-        # print(f"Child stdout: {stdout_output}, child stderr: {stderr_output}")
-
-        # Write the captured stdout and stderr to those of the parent
-        # Note that this is different from actually redirecting the output of the child, we save
-        # and explicitly write. If we had wanted to redirect, we'd get the stdout and stderr of
-        # the child, use os.dup() in the parent to 'realtime' redirect, and restored the parents
-        # original file handles for these streams after redirection is done.
-        # stdio isolation
-        if completed_process.stdout:
-            sys.stdout.write(completed_process.stdout.decode("utf-8"))
-        if completed_process.stderr:
-            sys.stderr.write(completed_process.stderr.decode("utf-8"))
+    # Handle whatever else may occur
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        sys.exit(ERROR_CODE)
 
     finally:
         # Change root back and restore original working dir
         os.chroot('/')
-        # os.chdir(old_cwd)
-
-        # Remove the temporary directory and all its contents
-        # This doesn't seem to work, as cc runs the tests in a docker, so not sure what's happening.
-        # shutil.rmtree(tmp_dir)
-
-        # Interesting that we can also do the following, instead of what is being done
-        # later on in the code.
-        # try:
-        #     completed_process.check_returncode()
-        # except subprocess.CalledProcessError as e:
-        #     # print(f"Child error {e.returncode}")
-        #     sys.exit(e.returncode)
 
         # Check if child was naughty. If it was, hang head in shame!
-        if completed_process is not None and completed_process.returncode > 0:
-            sys.exit(completed_process.returncode)
+        # We have this as a separate thing, because the codecrafters test child returns an exit code
+        # without actually failing or raising a CalledProcessError()
+        if subprocess_result is not None and subprocess_result.returncode > 0:
+            sys.exit(subprocess_result.returncode)
         sys.exit(0)
 
 
